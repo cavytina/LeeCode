776418459d8ff91276a677f305240144a96d34a0
cavy1010 <cavy1010@163.com>
2019-02-19 23:59:30 +0800

    add new files

---
+++ 28/ImplementStrStr/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementStrStr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solu = new Solution();
+            Console.WriteLine(solu.StrStr("aaa", "a"));
+
+            Console.ReadKey();
+        }
+
+        public class Solution
+        {
+            public int StrStr(string haystack, string needle)
+            {
+                int index = -1;
+                if (needle.Length == 0)
+                {
+                    index = 0;
+                }
+                else
+                {
+                    for (int i = 0; i <= haystack.Length - needle.Length; i++)
+                    {
+                        if (needle == haystack.Substring(i, needle.Length))
+                        {
+                            index = i;
+                            break;
+                        }
+                    }
+                }
+
+                return index;
+            }
+        }
+    }
+}
---
+++ 35/SearchInsertPosition/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchInsertPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solu = new Solution();
+            Console.WriteLine(solu.SearchInsert(new int[] { 1, 3, 5, 6 }, 0).ToString());
+
+            Console.ReadKey();
+        }
+
+        public class Solution
+        {
+            public int SearchInsert(int[] nums, int target)
+            {
+                int index = 0;
+                while (index < nums.Length)
+                {
+                    if (nums[index] >= target)
+                        break;
+                    index++;
+                }
+
+                return index;
+            }
+        }
+    }
+}
